064f06240bc4e62561a26f51dd8611b2416e7a6d
Edwin Silva Home <edwinsils@hotmail.com>
2022-12-18 23:21:04 -0500

    Finalization the stage 1/5 - Analyze the words - of the project Chuck Norris Cipher

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,7 +1,12 @@
 package chucknorris;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in) ;
+        System.out.println("Input a string:");
+        String string = scanner.nextLine();
+        for (int i = 0; i < string.length(); i++) {
+            System.out.print(string.charAt(i) + " ");
+        }
     }
 }

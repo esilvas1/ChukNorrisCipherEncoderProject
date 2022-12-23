7bd7ae3dc4f4dd69233ddba9390cda9e9bfd7b87
Edwin Silva Home <edwinsils@hotmail.com>
2022-12-23 12:51:50 -0500

    Finished stage 2/5

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -2,9 +2,22 @@ package chucknorris;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in) ;
+        String string = storeString();
+        convertToBinary(string);
+    }
+
+    public static String storeString() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Input a string:");
         String string = scanner.nextLine();
-        for (int i = 0; i < string.length(); i++) {
-            System.out.print(string.charAt(i) + " ");
+        return string;
+    }
+
+    public static void convertToBinary(String string) {
+        System.out.println("The result:");
+        for (int i = 0; i < string.length(); i++){
+            String binary = String.format("%07d",Integer.parseInt(Integer.toBinaryString(Integer.valueOf(string.charAt(i)))));
+            System.out.println(string.charAt(i) +" = " + binary);
         }
+    }
+}

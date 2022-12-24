7929d12269ac1d9493bd44d0d15948710b96bb0f
Edwin Silva Home <edwinsils@hotmail.com>
2022-12-24 02:42:31 -0500

    Finished the stage 3/5, solved

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -2,8 +2,11 @@ package chucknorris;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String string = storeString();
-        convertToBinary(string);
+        String string;
+        string = storeString();
+        string = convertToBinary(string);
+        string = encryptString(string);
+        printString(string);
     }
 
     public static String storeString() {
@@ -13,11 +16,57 @@ public class Main {
         return string;
     }
 
-    public static void convertToBinary(String string) {
-        System.out.println("The result:");
+    public static String convertToBinary(String string) {
+        StringBuilder sb = new StringBuilder();
+        String binary;
         for (int i = 0; i < string.length(); i++){
-            String binary = String.format("%07d",Integer.parseInt(Integer.toBinaryString(Integer.valueOf(string.charAt(i)))));
-            System.out.println(string.charAt(i) +" = " + binary);
+            sb.append(String.format("%07d",Integer.parseInt(Integer.toBinaryString(Integer.valueOf(string.charAt(i))))));
         }
+        binary = String.valueOf(sb);
+
+        return binary;
+    }
+
+    public static String encryptString(String string) {
+        StringBuilder sb = new StringBuilder();
+        String encriptedString = null;
+        String bit = null , nextBit;
+        int count;
+        for (int i = 0; i < string.length(); i++) {
+            count = 0;
+            for (int j = i; j < string.length(); j ++) {
+                bit = String.valueOf(string.charAt(i));
+                nextBit = String.valueOf(string.charAt(j));
+                if (bit.equals(nextBit)) count ++;
+                else break;
+            }
+
+            if (bit.equals("1")) {
+                sb.append("0");
+                sb.append(" ");
+                for (int j = 0; j < count; j++) {
+                    sb.append("0");
+                }
+                sb.append(" ");
+            }
+
+            if (bit.equals("0")) {
+                sb.append("00");
+                sb.append(" ");
+                for (int j = 0; j < count; j++) {
+                    sb.append("0");
+                }
+                sb.append(" ");
+            }
+
+            if (count != 1) i += (count - 1);
+        }
+        encriptedString = sb.toString();
+        return encriptedString;
+    }
+
+    public static void printString(String string) {
+        System.out.println("The result:");
+        System.out.println(string);
     }
 }
